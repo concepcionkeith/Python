d3e24e20d5ec55fb4063ffc61651f88393d12b57
concepcionkeith <keithconcepcion22@gmail.com>
2017-06-16 17:32:52 +0800

    Update Print_List_of_Even_Numbers.py

--- Print_List_of_Even_Numbers.py
+++ Print_List_of_Even_Numbers.py
@@ -1,2 +1,7 @@
-print [x for x in range(2, 100) if not x % 2]
-print range(2, 100, 2)
+x=0# set x to 0
+while (x<=100):#setx up to 100 so it will end
+    if(x%2 == 0):# gets the modulo 
+        
+        print(x)#prints the even nombers
+    x=x+1#add 1 to x
+
